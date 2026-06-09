bc3e904ead577d56d946520e46c2bcea034370a7
Manolo Revelo <manoale97@hotmail.com>
2026-06-09 01:40:15 -0500

    correciones

--- app/context.js
+++ app/context.js
@@ -4,14 +4,25 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 function getUsuarioFromToken(req) {
+
   const authHeader = req.headers.authorization;
-  if (!authHeader) return null;
-  
-  const token = authHeader.split(' ')[0];
+  if (!authHeader) {
+    return null;
+  }
+  if (!authHeader.startsWith('Bearer ')) {
+    return null;
+  }
+  const token =
+    authHeader.split(' ')[1];
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded =
+      jwt.verify(
+        token,
+        process.env.JWT_SECRET
+      );
     return decoded;
   } catch (error) {
+    console.error('JWT inválido');
     return null;
   }
 }
